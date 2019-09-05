f9a5679bfbc15803565d94f3de0941597fc11d34
Shucheng Yang <kelvin1020@foxmail.com>
2019-09-05 20:49:35 +0800

    01 draft

---
+++ 01.py
@@ -0,0 +1,18 @@
+Fc*(4*Ci*S2Beta*Cos(2*Phi) - 4*C2Beta*Ci*Sin(2*Phi) + e**6*((-65*Ci*S2Beta*Cos(2*Phi))/72. - (11*Ci*S2Beta*Cos(4*Phi - 6*Derivative(1)(Phi)))/45. + \
+(4096*Ci*S2Beta*Cos(8*Phi - 6*Derivative(1)(Phi)))/45. - (11*Ci*S2Beta*Cos(2*Phi - 4*Derivative(1)(Phi)))/120. - (2349*Ci*S2Beta*Cos(6*Phi - 4*Derivative(1)(Phi)))/20. + \
+(101*Ci*S2Beta*Cos(4*Phi - 2*Derivative(1)(Phi)))/3. + (65*C2Beta*Ci*Sin(2*Phi))/72. - (11*C2Beta*Ci*Sin(4*Phi - 6*Derivative(1)(Phi)))/45. - \
+(4096*C2Beta*Ci*Sin(8*Phi - 6*Derivative(1)(Phi)))/45. - (11*C2Beta*Ci*Sin(2*Phi - 4*Derivative(1)(Phi)))/120. + (2349*C2Beta*Ci*Sin(6*Phi - 4*Derivative(1)(Phi)))/20. - \
+(101*C2Beta*Ci*Sin(4*Phi - 2*Derivative(1)(Phi)))/3.) + e**2*\
+(-10*Ci*S2Beta*Cos(2*Phi) + 16*Ci*S2Beta*Cos(4*Phi - 2*Derivative(1)(Phi)) + 10*C2Beta*Ci*Sin(2*Phi) - 16*C2Beta*Ci*Sin(4*Phi - 2*Derivative(1)(Phi))) + \
+e**4*((23*Ci*S2Beta*Cos(2*Phi))/4. - (Ci*S2Beta*Cos(2*Phi - 4*Derivative(1)(Phi)))/4. + (81*Ci*S2Beta*Cos(6*Phi - 4*Derivative(1)(Phi)))/2. - \
+40*Ci*S2Beta*Cos(4*Phi - 2*Derivative(1)(Phi)) - (23*C2Beta*Ci*Sin(2*Phi))/4. - (C2Beta*Ci*Sin(2*Phi - 4*Derivative(1)(Phi)))/4. - \
+(81*C2Beta*Ci*Sin(6*Phi - 4*Derivative(1)(Phi)))/2. + 40*C2Beta*Ci*Sin(4*Phi - 2*Derivative(1)(Phi))) + \
+e**3*((-7*Ci*S2Beta*Cos(Phi - 3*Derivative(1)(Phi)))/24. + (625*Ci*S2Beta*Cos(5*Phi - 3*Derivative(1)(Phi)))/24. - (171*Ci*S2Beta*Cos(3*Phi - Derivative(1)(Phi)))/8. + \
+(13*Ci*S2Beta*Cos(Phi + Derivative(1)(Phi)))/8. - (7*C2Beta*Ci*Sin(Phi - 3*Derivative(1)(Phi)))/24. - (625*C2Beta*Ci*Sin(5*Phi - 3*Derivative(1)(Phi)))/24. + \
+(171*C2Beta*Ci*Sin(3*Phi - Derivative(1)(Phi)))/8. - (13*C2Beta*Ci*Sin(Phi + Derivative(1)(Phi)))/8.) + \
+e**5*((-153*Ci*S2Beta*Cos(3*Phi - 5*Derivative(1)(Phi)))/640. + (117649*Ci*S2Beta*Cos(7*Phi - 5*Derivative(1)(Phi)))/1920. - \
+(47*Ci*S2Beta*Cos(Phi - 3*Derivative(1)(Phi)))/384. - (26875*Ci*S2Beta*Cos(5*Phi - 3*Derivative(1)(Phi)))/384. + (963*Ci*S2Beta*Cos(3*Phi - Derivative(1)(Phi)))/64. + \
+(5*Ci*S2Beta*Cos(Phi + Derivative(1)(Phi)))/192. - (153*C2Beta*Ci*Sin(3*Phi - 5*Derivative(1)(Phi)))/640. - (117649*C2Beta*Ci*Sin(7*Phi - 5*Derivative(1)(Phi)))/1920. - \
+(47*C2Beta*Ci*Sin(Phi - 3*Derivative(1)(Phi)))/384. + (26875*C2Beta*Ci*Sin(5*Phi - 3*Derivative(1)(Phi)))/384. - (963*C2Beta*Ci*Sin(3*Phi - Derivative(1)(Phi)))/64. - \
+(5*C2Beta*Ci*Sin(Phi + Derivative(1)(Phi)))/192.) + e*(9*Ci*S2Beta*Cos(3*Phi - Derivative(1)(Phi)) - 3*Ci*S2Beta*Cos(Phi + Derivative(1)(Phi)) - \
+9*C2Beta*Ci*Sin(3*Phi - Derivative(1)(Phi)) + 3*C2Beta*Ci*Sin(Phi + Derivative(1)(Phi))))
