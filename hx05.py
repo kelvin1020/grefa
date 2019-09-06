4c65c574db380dc72564e1095c76ac5207ea26d8
恽倩芸 <yqy@yunqianyundeMacBook-Air.local>
2019-09-06 13:44:48 +0800

    new

---
+++ hx05.py
@@ -0,0 +1,75 @@
+\
+Fc*(((3*Ci*S1Beta*Si)/2. - Ci*e**2*S1Beta*Si + (5*Ci*e**4*S1Beta*Si)/128. - \
+(13*Ci*e**6*S1Beta*Si)/384.)*Cos(Phi) + \
+((-9*Ci*S3Beta*Si)/2. + 27*Ci*e**2*S3Beta*Si - (5319*Ci*e**4*S3Beta*Si)/128. + \
+(3141*Ci*e**6*S3Beta*Si)/128.)*Cos(3*Phi) + \
+(1233*Ci*e**6*S3Beta*Si*Cos(3*Phi - 6*Phip))/10240. + \
+(625*Ci*e**6*S1Beta*Si*Cos(5*Phi - 6*Phip))/6144. + \
+(117649*Ci*e**6*S1Beta*Si*Cos(7*Phi - 6*Phip))/10240. - \
+(4782969*Ci*e**6*S3Beta*Si*Cos(9*Phi - 6*Phip))/10240. + \
+(13*Ci*e**5*S3Beta*Si*Cos(2*Phi - 5*Phip))/120. + \
+(Ci*e**5*S1Beta*Si*Cos(4*Phi - 5*Phip))/15. + \
+(81*Ci*e**5*S1Beta*Si*Cos(6*Phi - 5*Phip))/10. - \
+(4096*Ci*e**5*S3Beta*Si*Cos(8*Phi - 5*Phip))/15. + \
+((25*Ci*e**4*S3Beta*Si)/256. + (179*Ci*e**6*S3Beta*Si)/3840.)*\
+Cos(Phi - 4*Phip) + \
+((9*Ci*e**4*S1Beta*Si)/256. - (63*Ci*e**6*S1Beta*Si)/1280.)*\
+Cos(3*Phi - 4*Phip) + \
+((4375*Ci*e**4*S1Beta*Si)/768. - (8125*Ci*e**6*S1Beta*Si)/768.)*\
+Cos(5*Phi - 4*Phip) + \
+((-117649*Ci*e**4*S3Beta*Si)/768. + (2705927*Ci*e**6*S3Beta*Si)/3840.)*\
+Cos(7*Phi - 4*Phip) - \
+(Ci*e**5*S1Beta*Si*Cos(2*Phi - 3*Phip))/24. + \
+(4*Ci*e**3*S1Beta*Si - (19*Ci*e**5*S1Beta*Si)/3.)*Cos(4*Phi - 3*Phip) + \
+(-81*Ci*e**3*S3Beta*Si + (729*Ci*e**5*S3Beta*Si)/2.)*Cos(6*Phi - 3*Phip) + \
+(-(Ci*e**2*S1Beta*Si)/16. - (5*Ci*e**4*S1Beta*Si)/96. - (187*Ci*e**6*S1Beta*Si)/6144.)*\
+Cos(Phi - 2*Phip) + \
+((45*Ci*e**2*S1Beta*Si)/16. - (117*Ci*e**4*S1Beta*Si)/32. + \
+(2961*Ci*e**6*S1Beta*Si)/2048.)*Cos(3*Phi - 2*Phip) + \
+((-625*Ci*e**2*S3Beta*Si)/16. + (5625*Ci*e**4*S3Beta*Si)/32. - \
+(1746875*Ci*e**6*S3Beta*Si)/6144.)*Cos(5*Phi - 2*Phip) + \
+(2*Ci*e*S1Beta*Si - 2*Ci*e**3*S1Beta*Si + (11*Ci*e**5*S1Beta*Si)/24.)*\
+Cos(2*Phi - Phip) + \
+(-16*Ci*e*S3Beta*Si + 76*Ci*e**3*S3Beta*Si - (352*Ci*e**5*S3Beta*Si)/3.)*\
+Cos(4*Phi - Phip) + \
+(6*Ci*e*S3Beta*Si - 11*Ci*e**3*S3Beta*Si + (133*Ci*e**5*S3Beta*Si)/24.)*\
+Cos(2*Phi + Phip) + \
+((-19*Ci*e**2*S3Beta*Si)/16. + (65*Ci*e**4*S3Beta*Si)/96. + \
+(19*Ci*e**6*S3Beta*Si)/6144.)*Cos(Phi + 2*Phip) + \
+((-3*C1Beta*Ci*Si)/2. + C1Beta*Ci*e**2*Si - (5*C1Beta*Ci*e**4*Si)/128. + \
+(13*C1Beta*Ci*e**6*Si)/384.)*Sin(Phi) + \
+((9*C3Beta*Ci*Si)/2. - 27*C3Beta*Ci*e**2*Si + (5319*C3Beta*Ci*e**4*Si)/128. - \
+(3141*C3Beta*Ci*e**6*Si)/128.)*Sin(3*Phi) + \
+(1233*C3Beta*Ci*e**6*Si*Sin(3*Phi - 6*Phip))/10240. + \
+(625*C1Beta*Ci*e**6*Si*Sin(5*Phi - 6*Phip))/6144. - \
+(117649*C1Beta*Ci*e**6*Si*Sin(7*Phi - 6*Phip))/10240. + \
+(4782969*C3Beta*Ci*e**6*Si*Sin(9*Phi - 6*Phip))/10240. + \
+(13*C3Beta*Ci*e**5*Si*Sin(2*Phi - 5*Phip))/120. + \
+(C1Beta*Ci*e**5*Si*Sin(4*Phi - 5*Phip))/15. - \
+(81*C1Beta*Ci*e**5*Si*Sin(6*Phi - 5*Phip))/10. + \
+(4096*C3Beta*Ci*e**5*Si*Sin(8*Phi - 5*Phip))/15. + \
+((25*C3Beta*Ci*e**4*Si)/256. + (179*C3Beta*Ci*e**6*Si)/3840.)*\
+Sin(Phi - 4*Phip) + \
+((9*C1Beta*Ci*e**4*Si)/256. - (63*C1Beta*Ci*e**6*Si)/1280.)*\
+Sin(3*Phi - 4*Phip) + \
+((-4375*C1Beta*Ci*e**4*Si)/768. + (8125*C1Beta*Ci*e**6*Si)/768.)*\
+Sin(5*Phi - 4*Phip) + \
+((117649*C3Beta*Ci*e**4*Si)/768. - (2705927*C3Beta*Ci*e**6*Si)/3840.)*\
+Sin(7*Phi - 4*Phip) - \
+(C1Beta*Ci*e**5*Si*Sin(2*Phi - 3*Phip))/24. + \
+(-4*C1Beta*Ci*e**3*Si + (19*C1Beta*Ci*e**5*Si)/3.)*Sin(4*Phi - 3*Phip) + \
+(81*C3Beta*Ci*e**3*Si - (729*C3Beta*Ci*e**5*Si)/2.)*Sin(6*Phi - 3*Phip) + \
+(-(C1Beta*Ci*e**2*Si)/16. - (5*C1Beta*Ci*e**4*Si)/96. - (187*C1Beta*Ci*e**6*Si)/6144.)*\
+Sin(Phi - 2*Phip) + \
+((-45*C1Beta*Ci*e**2*Si)/16. + (117*C1Beta*Ci*e**4*Si)/32. - \
+(2961*C1Beta*Ci*e**6*Si)/2048.)*Sin(3*Phi - 2*Phip) + \
+((625*C3Beta*Ci*e**2*Si)/16. - (5625*C3Beta*Ci*e**4*Si)/32. + \
+(1746875*C3Beta*Ci*e**6*Si)/6144.)*Sin(5*Phi - 2*Phip) + \
+(-2*C1Beta*Ci*e*Si + 2*C1Beta*Ci*e**3*Si - (11*C1Beta*Ci*e**5*Si)/24.)*\
+Sin(2*Phi - Phip) + \
+(16*C3Beta*Ci*e*Si - 76*C3Beta*Ci*e**3*Si + (352*C3Beta*Ci*e**5*Si)/3.)*\
+Sin(4*Phi - Phip) + \
+(-6*C3Beta*Ci*e*Si + 11*C3Beta*Ci*e**3*Si - (133*C3Beta*Ci*e**5*Si)/24.)*\
+Sin(2*Phi + Phip) + \
+((19*C3Beta*Ci*e**2*Si)/16. - (65*C3Beta*Ci*e**4*Si)/96. - 
+(19*C3Beta*Ci*e**6*Si)/6144.)*Sin(Phi + 2*Phip))
